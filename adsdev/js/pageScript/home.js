c7b5cd6201e86d02f75b131f31f6ad4ccd20c387
Adisakz <film.asa17@gmail.com>
2024-09-02 21:13:05 +0700

    Add Animation

---
+++ adsdev/js/pageScript/home.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        let elements = document.querySelectorAll('.content-works .row .work');
+        elements.forEach(function (element) {
+            let position = element.getBoundingClientRect();
+
+            // เช็คว่าองค์ประกอบอยู่ใน viewport
+            if (position.top < window.innerHeight && position.bottom >= 0) {
+                element.style.animationName = 'moveLoadUp';
+                element.style.animationDuration = '1s';
+            }
+        });
+    });
+    
+});
